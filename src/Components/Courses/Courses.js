7ccf95f9cca09913118c34d6848ca98458dc7fa7
parthusun8 <parthusun8@gmail.com>
2022-03-09 23:52:02 +0530

    Login page added and Signup page added

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import "./Courses.css";
+import { Link } from "react-router-dom";
 
 function Courses() {
-    return (
-        <div>
-            COURSE PAGE
-        </div>
-    )
+  return (
+    <div className="courses">
+      <Navbar />
+      <div className="search-bar">
+        <span>COURSES</span>
+        <input type="text" name="" id="search" autoComplete="off" />
+      </div>
+      <div className="courses-container">
+        <Course />
+        <Course />
+      </div>
+    </div>
+  );
 }
 
-export default Courses
+export default Courses;
+
+function Navbar() {
+  return (
+    <div className="nav">
+      <h1>GyaanDaan</h1>
+      <div className="options">
+        <Link to={"/student"}>
+          <button className="btn">PROFILE</button>
+        </Link>
+        <button className="btn">HISTORY</button>
+        <Link to={"/dashboard"}>
+          <button className="btn">MYCOURSES</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function Course() {
+  return (
+    <div className="container">
+      <div className="course">
+        <span>NAME</span>
+        <img src="https://img.icons8.com/ios-glyphs/30/000000/star--v1.png" />
+      </div>
+      <div className="course">
+        <span>NAME</span>
+        <img src="https://img.icons8.com/ios-glyphs/30/000000/star--v1.png" />
+      </div>
+      <div className="course">
+        <span>NAME</span>
+        <img src="https://img.icons8.com/ios-glyphs/30/000000/star--v1.png" />
+      </div>
+      <div className="course">
+        <span>NAME</span>
+        <img src="https://img.icons8.com/ios-glyphs/30/000000/star--v1.png" />
+      </div>
+    </div>
+  );
+}
