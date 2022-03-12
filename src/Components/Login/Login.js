4dc33ce0e794789346d3729c90ccf034c442339c
parthusun8 <parthusun8@gmail.com>
2022-03-12 16:05:26 +0530

    Many Changes done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../Firebase/firebase";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let navigate = useNavigate();
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //SIGNED IN
+        const user = userCredential.user;
+        if (user) {
+          console.log(user);
+        }
+      })
+      .catch((error) => {
+        const errorcode = error.code;
+        const errorMessage = error.message;
+        alert(errorcode, errorMessage);
+      });
+  };
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("Logged In hai");
+      navigate("/courses");
+    } else {
+      console.log("Logout");
+    }
+  });
   return (
     <div className="login">
       <h1>LogIn</h1>
       <form>
-        <input type="email" placeholder="Email Id" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="email"
+          placeholder="Email Id"
+          autoComplete="off"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          autoComplete="off"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={signIn}>
+          Login
+        </button>
       </form>
 
-      <Link to={"/courses"}>
-        <button>Login</button>
-      </Link>
       <Link to={"/signup"}>
         <span>Sign up instead?</span>
       </Link>
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,16 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signup.css";
 import { Link } from "react-router-dom";
+import { auth } from "../Firebase/firebase";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //SIGNED IN
+        const user = userCredential.user;
+        if (user) {
+          alert("Registered");
+        }
+      })
+      .catch((error) => {
+        const errorcode = error.code;
+        const errorMessage = error.message;
+        alert(errorcode, errorMessage);
+      });
+  };
+
   return (
     <div className="signup">
       <h1>SignUp</h1>
       <form>
-        <input type="text" name="" id="" placeholder="First Name" />
-        <input type="text" name="" id="" placeholder="Last Name" />
-        <input type="email" placeholder="Email Id" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="First Name"
+          autoComplete="off"
+          required
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Last Name"
+          autoComplete="off"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email Id"
+          autoComplete="off"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          autoComplete="off"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <label htmlFor="roles">SELECT ROLE</label>
         <select name="roles" id="roles">
           <option value="" selected="selected">
@@ -19,11 +71,9 @@ function SignUp() {
           <option value="teacher">Teacher</option>
           <option value="student">Student</option>
         </select>
+        <button onClick={register}>SignUp</button>
       </form>
 
-      <Link to={"/courses"}>
-        <button>SignUp</button>
-      </Link>
       <Link to={"/login"}>
         <span>Log In instead?</span>
       </Link>
